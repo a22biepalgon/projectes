8fc05fe5426ed835acf63814c1deb62fdb1d1d35
biel_palomar <palomarbiel@gmail.com>
2024-01-10 22:21:34 +0100

    QUE ES AIXO

--- VuetifyPiniaElectron/electronVuetify/src/components/charts.vue
+++ VuetifyPiniaElectron/electronVuetify/src/components/charts.vue
@@ -1,56 +1,64 @@
 <template>
-{{vots}}
- <Bar v-if="this.loaded"
-    id="my-chart-id"
-    :options="chartOptions"
-    :data="chartData"
-  />
+  {{ vots }}
+  <Bar v-if="this.loaded" id="my-chart-id" :options="chartOptions" :data="chartData" :key="chartKey" />
 </template>
 <script>
 import { useAppStore } from '@/store/app'
 const pinia = useAppStore();
 import { Bar } from 'vue-chartjs'
-  import { watch } from 'vue'
+import { watch } from 'vue'
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
 import { getVotacions } from '@/communicationsManager.js'
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
-watch(
-  () => pinia.$state.infoVotos.votos,
-  (votos) => {
-    console.log(`count is: ${votos}`)
-  }, { deep: true }
-)
+
 export default {
   name: 'BarChart',
   components: { Bar },
   data() {
     return {
-        loaded: false,
+      loaded: false,
       chartData: {
-        labels: [ 'Vots 1', 'Vots 2', 'Vots 3', 'Vots 4' ],
-        datasets: [ { data: [] } ]
+        labels: ['Vots 1', 'Vots 2', 'Vots 3', 'Vots 4'],
+        datasets: [{ data: [] }]
       },
       chartOptions: {
         responsive: true
-      }
+      },
+      chartKey: 0
     }
   },
   methods: {
-    async recarregar(){
-    this.chartData.datasets[0].data = await getVotacions()
-    this.loaded = true
+    async recarregar() {
+      this.chartData.datasets[0].data = await getVotacions()
+      this.loaded = true
+      this.chartkey += 1;
+    },
+    recarregar2(votos) {
+      console.log(`count is: ${votos}`)
+      this.chartKey += 1;
+      this.chartData.datasets[0].data = votos
     }
   },
-  created(){
+  created() {
     this.recarregar()
   },
-  computed:{
-    vots(){
-        return pinia.$state.infoVotos.votos
+  computed: {
+    vots() {
+      return pinia.$state.infoVotos.votos
     },
   },
 
-  
+  // setup() {
+  //   watch(
+  //     () => pinia.$state.infoVotos.votos,
+  //     (votos) => {
+
+  //     }, { deep: true }
+  //   )
+  // }
+
+
 }
+
 </script>
